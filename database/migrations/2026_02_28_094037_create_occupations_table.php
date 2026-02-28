86e8bfcc9509217a637e52e49f128e735a3b17f4
Md. Rakibul Islam <raaakib0@gmail.com>
2026-02-28 18:56:39 +0600

    api structure

--- database/migrations/2026_02_28_094037_create_occupations_table.php
+++ database/migrations/2026_02_28_094037_create_occupations_table.php
@@ -12,7 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('occupations', function (Blueprint $table) {
-            $table->id();
+           $table->id();
+
+            $table->string('name')->unique();
+
             $table->timestamps();
         });
     }
